61d7d2de368fd25aef86a8ebd1f54296daa1ac10
mike-linshi <mike.linshi@flatironschool.com>
2015-10-11 18:47:12 -0400

    Done.

--- countdown_with_sleep.rb
+++ countdown_with_sleep.rb
@@ -1,10 +1,11 @@
-#write your code here
-
-def countdown(num)
-  countdown_number = num 
+def countdown_with_sleep(num)
+  countdown_number = num
   while countdown_number > 0
     puts "#{countdown_number} SECOND(S)!"
     countdown_number -= 1
+    sleep 1
   end
   return "HAPPY NEW YEAR!"
 end
+
+countdown_with_sleep(5)
